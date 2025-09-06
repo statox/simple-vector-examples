f6caf2455f05267dc2e0f86e3c15c37a1725deb9
statox <me@statox.fr>
2025-09-06 15:00:49 +0200

    CI - Add Github pages deployment workflow

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,28 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const REPO_NAME = '/simple-vector-examples';
+const prod = process.env.ENV === 'prod';
+const base = prod ? REPO_NAME : '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
     preprocess: [vitePreprocess(), mdsvex()],
-    kit: { adapter: adapter() },
+    kit: {
+        adapter: adapter({
+            pages: 'docs',
+            assets: 'docs',
+            precompress: false,
+            fallback: '404.html',
+            domain: '',
+            jekyll: false
+        }),
+        paths: {
+            base
+        }
+    },
     extensions: ['.svelte', '.svx']
 };
 
