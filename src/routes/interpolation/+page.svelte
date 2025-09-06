641c1f7b90b95c8734eda5e4f26caac3d7a9959e
statox <me@statox.fr>
2025-09-06 18:05:03 +0200

    Interpolations - Add mixX, mixY and projectOnto

--- src/routes/interpolation/+page.svelte
+++ src/routes/interpolation/+page.svelte
@@ -4,9 +4,12 @@
 
     let v1: Vector = $state(new Vector(0, 100));
     let v2: Vector = $state(new Vector(100, 0));
-    let v3: Vector = $derived(v1.clone().mix(v2, 0.5));
+    let mix: Vector = $derived(v1.clone().mix(v2, 0.5));
+    let mixX: Vector = $derived(v1.clone().mixX(v2, 0.5));
+    let mixY: Vector = $derived(v1.clone().mixY(v2, 0.5));
+    let projectOnto: Vector = $derived(v1.clone().projectOnto(v2));
 
-    const vectors = $derived([
+    const baseVectors = $derived([
         {
             name: 'v1',
             vec: v1,
@@ -24,14 +27,45 @@
                 v2 = newVector.clone();
             },
             isDraggable: true
-        },
+        }
+    ]);
+
+    const vectorsMix = $derived([
+        ...baseVectors,
         {
-            name: 'v3',
-            vec: v3,
-            color: '#0000FF',
-            onUpdate: (newVector: Vector) => {
-                v3 = newVector.clone();
-            },
+            name: 'v1.mix(v2, 0.5)',
+            vec: mix,
+            color: '#6e6ef4',
+            isDraggable: false
+        }
+    ]);
+
+    const vectorsMixX = $derived([
+        ...baseVectors,
+        {
+            name: 'v1.mixX(v2, 0.5)',
+            vec: mixX,
+            color: '#f46ef4',
+            isDraggable: false
+        }
+    ]);
+
+    const vectorsMixY = $derived([
+        ...baseVectors,
+        {
+            name: 'v1.mixY(v2, 0.5)',
+            vec: mixY,
+            color: '#b16ef4',
+            isDraggable: false
+        }
+    ]);
+
+    const vectorsProjectOnto = $derived([
+        ...baseVectors,
+        {
+            name: 'v1.projectOnto(v2)',
+            vec: projectOnto,
+            color: '#6eb1f4',
             isDraggable: false
         }
     ]);
@@ -39,4 +73,29 @@
 
 <h2>Interpolation</h2>
 
-<VectorsDisplay {vectors}></VectorsDisplay>
+<div class="container">
+    <div>
+        <h3><code>v1.mix(v2, 0.5)</code></h3>
+        <VectorsDisplay vectors={vectorsMix}></VectorsDisplay>
+    </div>
+    <div>
+        <h3><code>v1.mixX(v2, 0.5)</code></h3>
+        <VectorsDisplay vectors={vectorsMixX}></VectorsDisplay>
+    </div>
+    <div>
+        <h3><code>v1.mixY(v2, 0.5)</code></h3>
+        <VectorsDisplay vectors={vectorsMixY}></VectorsDisplay>
+    </div>
+    <div>
+        <h3><code>v1.projectOnto(v2)</code></h3>
+        <VectorsDisplay vectors={vectorsProjectOnto}></VectorsDisplay>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+</style>
