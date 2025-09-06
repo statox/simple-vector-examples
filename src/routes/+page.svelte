bf849631d2c72696e1eab3dc18fe697e864839ec
statox <me@statox.fr>
2025-09-06 22:09:23 +0200

    Presentation - Create component for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
 <script>
-    import { resolve } from '$app/paths';
+    import { Presentation } from '$lib/components/Presentation';
 </script>
 
-<ul>
-    <li><a href={resolve('/angle')}>Angle examples</a></li>
-    <li><a href={resolve('/angleWith')}>AngleWith examples</a></li>
-    <li><a href={resolve('/distance')}>Distance examples</a></li>
-    <li><a href={resolve('/interpolation')}>Interpolation examples</a></li>
-    <li><a href={resolve('/product')}>Product examples</a></li>
-</ul>
+<Presentation />
