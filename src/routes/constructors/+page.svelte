b14d4de46d75dfe4352c1c68d9d7b4f6a66a0140
statox <me@statox.fr>
2025-09-07 23:04:54 +0200

    Constructors - Add page

---
+++ src/routes/constructors/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import { VectorsDisplay } from '$lib/components/VectorsDisplay';
+    import { Vector } from 'simple-vector';
+
+    let v: Vector = $state(new Vector(0, 2.5));
+
+    const resultingArray = $derived.by(() => {
+        const a = v.toArray();
+        return `[${a[0]}, ${a[1]}]`;
+    });
+    const resultingObject = $derived.by(() => {
+        const o = v.toObject();
+        return `{x : ${o.x}, y: ${o.y}}`;
+    });
+    const resultingPolar = $derived.by(() => {
+        const p = v.toPolar();
+        return `{r : ${p.r}, theta: ${p.theta}}`;
+    });
+
+    const vectors = $derived([
+        {
+            name: 'v',
+            vec: v,
+            color: '#00FF00',
+            onUpdate: (newVector: Vector) => {
+                v = newVector.clone().fixPrecision(1);
+            },
+            isDraggable: true
+        }
+    ]);
+</script>
+
+<h2>Constructors</h2>
+
+<VectorsDisplay grid={{ size: 10, graduation: 1 }} {vectors}></VectorsDisplay>
+
+<div class="results">
+    <div><code>v.toArray()</code></div>
+    <div><code>{resultingArray}</code></div>
+
+    <div><code>v.toObject()</code></div>
+    <div><code>{resultingObject}</code></div>
+
+    <div><code>v.toPolar()</code></div>
+    <div><code>{resultingPolar}</code></div>
+</div>
+
+<style>
+    .results {
+        display: grid;
+        grid-template-columns: 150px 1fr;
+    }
+</style>
