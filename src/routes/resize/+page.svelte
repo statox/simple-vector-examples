ef8e3e0636bcb3606c41452fe425ca3f2fa17351
statox <me@statox.fr>
2025-09-10 20:43:25 +0200

    resize - use same format as other pages

--- src/routes/resize/+page.svelte
+++ src/routes/resize/+page.svelte
@@ -1,8 +1,79 @@
 <script lang="ts">
     import { PageTitle } from '$lib/components/PageTitle';
-    import Length from './length.svelte';
+    import { VectorsDisplay } from '$lib/components/VectorsDisplay';
+    import { Vector } from 'simple-vector';
+
+    let resizedMag = $state(2);
+
+    let v1: Vector = $state(new Vector(3, 3));
+    let normalized: Vector = $derived.by(() => {
+        try {
+            return v1.clone().normalize();
+        } catch {
+            return v1.clone().zero();
+        }
+    });
+    let resized: Vector = $derived.by(() => {
+        try {
+            return v1.clone().resize(resizedMag);
+        } catch {
+            return v1.clone().zero();
+        }
+    });
+
+    const baseVectors = $derived([
+        {
+            name: 'v1',
+            vec: v1,
+            color: '#00FF00',
+            onUpdate: (newVector: Vector) => {
+                v1 = newVector.clone().fixPrecision(1);
+            },
+            isDraggable: true
+        }
+    ]);
+
+    const vectorsNormalize = $derived([
+        ...baseVectors,
+        {
+            name: `v1.normalize()`,
+            vec: normalized,
+            color: '#6e6ef4',
+            isDraggable: false
+        }
+    ]);
+
+    const vectorsResize = $derived([
+        ...baseVectors,
+        {
+            name: `v1.resize(${resizedMag})`,
+            vec: resized,
+            color: '#b16ef4',
+            isDraggable: false
+        }
+    ]);
+
+    const grid = { size: 10, graduation: 1 };
 </script>
 
 <PageTitle title="Resize methods" />
 
-<Length />
+<div class="results">
+    <div>
+        <h3><code>v1.resize({resizedMag})</code></h3>
+        <span>Resized magnitude</span><input type="number" bind:value={resizedMag} />
+        <VectorsDisplay {grid} vectors={vectorsResize}></VectorsDisplay>
+    </div>
+    <div>
+        <h3><code>v1.normalize()</code></h3>
+        <VectorsDisplay {grid} vectors={vectorsNormalize}></VectorsDisplay>
+    </div>
+</div>
+
+<style>
+    .results {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+</style>
