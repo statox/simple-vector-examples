2815dc9a2e9c42360cf56c9cf7d910c83228adb8
statox <me@statox.fr>
2025-09-11 17:14:54 +0200

    Rotation - Add explanation and tweak display

--- src/routes/rotation/+page.svelte
+++ src/routes/rotation/+page.svelte
@@ -50,52 +50,69 @@
 
 <PageTitle title="Rotation methods" />
 
+<p>
+    The rotation methods take as a parameter a number representing an angle. To let you test the
+    methods taking an angle in radians, this page lets you input parameters as strings which are
+    evaluated with <a
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval"
+        ><code>eval()</code></a
+    >.
+</p>
+<p>
+    This way you can use a number or a valid javascript expression like <code>Math.PI/3</code> as you
+    would in your code.
+</p>
+<p>
+    Then you can drag the vector to its initial position then click <button disabled>rotate</button>
+    to see how the methods modify the vector.
+</p>
+
 <VectorsDisplay grid={{ size: 10, graduation: 1 }} {vectors}></VectorsDisplay>
 
 <div class="results">
-    <div>
+    <div class="result-code">
         <code>
             v.rotateBy(
             <input class="inline-input" type="string" bind:value={rotateByAngle} />
             )
         </code>
     </div>
-    <div>
+    <div class="result-value">
         <button onclick={() => (v1 = v1.clone().rotateBy(eval(rotateByAngle)))}>Rotate</button>
     </div>
 
-    <div>
+    <div class="result-code">
         <code>
             v.rotateByDeg(
             <input class="inline-input" type="string" bind:value={rotateByDegAngle} />
             )
         </code>
     </div>
-    <div>
+    <div class="result-value">
         <button onclick={() => (v1 = v1.clone().rotateByDeg(Number(rotateByDegAngle)))}
             >Rotate</button
         >
     </div>
 
-    <div>
+    <div class="result-code">
         <code>
             v.rotateTo(
             <input class="inline-input" type="string" bind:value={rotateToAngle} />
             )
         </code>
     </div>
-    <div>
+    <div class="result-value">
         <button onclick={() => (v1 = v1.clone().rotateTo(eval(rotateToAngle)))}>Rotate</button>
     </div>
 
-    <div>
+    <div class="result-code">
         <code>
             v.rotateToDeg(
             <input class="inline-input" type="string" bind:value={rotateToDegAngle} />
             )
         </code>
     </div>
-    <div>
+    <div class="result-value">
         <button onclick={() => (v1 = v1.clone().rotateToDeg(Number(rotateToDegAngle)))}
             >Rotate</button
         >
@@ -107,27 +124,27 @@
 <VectorsDisplay grid={{ size: 10, graduation: 1 }} vectors={vectors2}></VectorsDisplay>
 
 <div class="results">
-    <div>
+    <div class="result-code">
         <code>
             v1.rotateTowards(v2,
             <input class="inline-input" type="string" bind:value={rotateTowardsAngle} />
             )
         </code>
     </div>
-    <div>
+    <div class="result-value">
         <button onclick={() => (v1 = v1.clone().rotateTowards(v2, eval(rotateTowardsAngle)))}
             >Rotate</button
         >
     </div>
 
-    <div>
+    <div class="result-code">
         <code>
-            v.rotateTowardsDeg(v2
+            v.rotateTowardsDeg(v2,
             <input class="inline-input" type="string" bind:value={rotateTowardsDegAngle} />
             )
         </code>
     </div>
-    <div>
+    <div class="result-value">
         <button
             onclick={() => (v1 = v1.clone().rotateTowardsDeg(v2, Number(rotateTowardsDegAngle)))}
             >Rotate</button
@@ -138,10 +155,21 @@
 <style>
     .results {
         display: grid;
-        grid-template-columns: 500px 1fr;
+        grid-template-columns: max-content 1fr;
+        column-gap: 1rem;
 
-        .inline-input {
-            display: inline;
+        margin-top: var(--gap);
+
+        .result-code {
+            color: var(--info-fg);
+        }
+
+        @media (width<500px) {
+            grid-template-columns: 1fr;
+
+            .result-value {
+                padding-left: 1rem;
+            }
         }
     }
 </style>
